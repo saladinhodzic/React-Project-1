6bd0deedbd289da77318da112b5fb8929674a70c
saladinhodzic <hsaladin06@gmail.com>
2024-05-29 20:24:13 +0200

    Improving the Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,23 @@
 import { FaInstagram } from "react-icons/fa";
 import { CiFacebook } from "react-icons/ci";
 import { CiTwitter } from "react-icons/ci";
+import "./Footer.css";
 
 export function Footer() {
   return (
     <footer>
-      <div>
+      <div className="info">
         <h1>SAKIley</h1>
         <h5>Stevana Nemanje 47</h5>
         <h5>Novi Pazar 36300</h5>
         <h5>Phone:063-842-42-88</h5>
         <h5>E-mail:hsaladin06@gmail.com</h5>
       </div>
-      <div style={{ width: "2em" }}>
-        <FaInstagram />
-        <CiFacebook />
-        <CiTwitter />
+      <div className="icons">
+        <FaInstagram size="50px" className="instagram" />
+        <CiFacebook size="50px" />
+        <CiTwitter size="50px" />
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
     </footer>
   );
 }
