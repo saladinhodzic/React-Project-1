e8145e9a6629275930551267747636042d177eb8
saladinhodzic <hsaladin06@gmail.com>
2024-06-14 21:14:22 +0200

    Pagination numbers working

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,10 +1,30 @@
 import "./Pagination.css";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
-export function Pagination() {
+export function Pagination({ brojStranica, setPage }) {
+  const niz = [];
+  for (let i = 1; i <= brojStranica; i++) {
+    niz.push(i);
+  }
   return (
     <div className="pagination">
-      <MdKeyboardArrowLeft className="pagination-btn" />
-      <MdKeyboardArrowRight className="pagination-btn" />
+      <button className="pagination-btn">
+        <MdKeyboardArrowLeft />
+      </button>
+      {niz.map((value, index) => {
+        return (
+          <button
+            onClick={() => setPage(value)}
+            key={index}
+            style={{ fontSize: "1.25em" }}
+            className="pagination-btn"
+          >
+            {value}
+          </button>
+        );
+      })}
+      <button className="pagination-btn">
+        <MdKeyboardArrowRight />
+      </button>
     </div>
   );
 }
--- src/pages/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels.jsx
@@ -2,35 +2,46 @@ import hotels from "../../common/hotels.json";
 import { Card } from "../../components/Card/Card";
 import { NewNavbar } from "../../components/NewNavbar/NewNavbar";
 import { Pagination } from "../../components/Pagination/Pagination";
+import { useState } from "react";
 import "./Hotels.css";
 
 export function Catalog() {
-  // const { id } = useParams();
+  // 8 hotela po stranici
+  // koliko imam ukupno stranica
+  const [page, setPage] = useState(1);
+  const brojHotela = hotels.length;
+  const brojHotelaPoStranici = 8;
+  const brojStranica = Math.ceil(brojHotela / brojHotelaPoStranici);
   return (
     <div className="hotels-cards">
       <NewNavbar />
       <div className="katalog">
-        {hotels.map((value) => {
-          return (
-            <Card
-              key={value["id"]}
-              slika={value["imageUrl"]}
-              hotelName={value["title"]}
-              country={value["content"]}
-              price={
-                <>
-                  ${value["total"]}{" "}
-                  <del style={{ fontSize: "0.75em" }}>$569</del>
-                </>
-              }
-              score={value["rating"]}
-              reviews={425}
-              onclick={() => (window.location.href = `/hotels/${value.id}`)}
-            />
-          );
-        })}
+        {hotels
+          .map((value) => {
+            return (
+              <Card
+                key={value["id"]}
+                slika={value["imageUrl"]}
+                hotelName={value["title"]}
+                country={value["content"]}
+                price={
+                  <>
+                    ${value["total"]}{" "}
+                    <del style={{ fontSize: "0.75em" }}>$569</del>
+                  </>
+                }
+                score={value["rating"]}
+                reviews={425}
+                onclick={() => (window.location.href = `/hotels/${value.id}`)}
+              />
+            );
+          })
+          .slice(
+            brojHotelaPoStranici * (page - 1),
+            brojHotelaPoStranici * page
+          )}
       </div>
-      <Pagination />
+      <Pagination brojStranica={brojStranica} setPage={setPage} />
     </div>
   );
 }
