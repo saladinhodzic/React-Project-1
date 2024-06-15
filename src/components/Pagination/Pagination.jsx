fb20ad2bfd04880c2cf9fdc03f865fb7b665b1b3
saladinhodzic <hsaladin06@gmail.com>
2024-06-15 09:09:21 +0200

    Making the pagination scroll on top of the page

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,19 +1,35 @@
 import "./Pagination.css";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { useState, useEffect } from "react";
 export function Pagination({ brojStranica, setPage, page }) {
   const niz = [];
   for (let i = 1; i <= brojStranica; i++) {
     niz.push(i);
   }
+
+  document.querySelectorAll(".pagination-btn").forEach((value) => {
+    value.addEventListener(
+      "click",
+      () => (window.onscroll = document.documentElement.scrollTop = 0)
+    );
+  });
+
   return (
     <div className="pagination">
-      <button className="pagination-btn" onClick={() => setPage(page - 1)}>
+      <button
+        className="pagination-btn"
+        onClick={() => {
+          setPage(page - 1);
+        }}
+      >
         <MdKeyboardArrowLeft />
       </button>
       {niz.map((value, index) => {
         return (
           <button
-            onClick={() => setPage(value)}
+            onClick={() => {
+              setPage(value);
+            }}
             key={index}
             style={{ fontSize: "1.25em" }}
             className="pagination-btn"
@@ -22,7 +38,12 @@ export function Pagination({ brojStranica, setPage, page }) {
           </button>
         );
       })}
-      <button className="pagination-btn" onClick={() => setPage(page + 1)}>
+      <button
+        className="pagination-btn"
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
         <MdKeyboardArrowRight />
       </button>
     </div>
