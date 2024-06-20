f7c5aa03ef72d2282fce4ce11bea17fc2d00164d
saladinhodzic <hsaladin06@gmail.com>
2024-06-20 17:15:16 +0200

    Getting little bit closer

--- src/pages/Quotes/Quotes.jsx
+++ src/pages/Quotes/Quotes.jsx
@@ -5,44 +5,48 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { NewNavbar } from "../../components/NewNavbar/NewNavbar";
+import { useEffect, useState } from "react";
 
 function Quotes() {
-  const data = async () => {
-    // const [podaci, setPodaci] = useState([]);
-    try {
-      const url = await fetch("https://api.quotable.io/quotes?page=1");
-      const data = await url.json();
-      const results = data.results;
-      return results;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  data();
+  const [podaci, setPodaci] = useState([]);
+  useEffect(() => {
+    fetch("https://api.quotable.io/quotes?page=1")
+      .then((url) => {
+        return url.json();
+      })
+      .then((data) => {
+        setPodaci(data.results);
+      });
+  }, []);
+  console.log(podaci);
   return (
     <>
       <NewNavbar />
-      <Card sx={{ width: 275, boxShadow: "0px 10px 20px 5px black" }}>
-        <CardContent>
-          <Typography
-            sx={{ fontSize: 14 }}
-            color="text.secondary"
-            gutterBottom
-          ></Typography>
-          <Typography variant="h5" component="div"></Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
-          </Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+      {podaci.map((value) => {
+        <Card sx={{ width: 275, boxShadow: "0px 10px 20px 5px black" }}>
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              {value.author}
+            </Typography>
+            <Typography variant="h5" component="div"></Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              adjective
+            </Typography>
+            <Typography variant="body2">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>;
+      })}
     </>
   );
 }
