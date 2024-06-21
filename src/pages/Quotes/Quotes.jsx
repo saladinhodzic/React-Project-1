830b17c926eeaa05d12577fd56629bc2577477be
saladinhodzic <hsaladin06@gmail.com>
2024-06-21 19:00:40 +0200

    Making the card with material ui

--- src/pages/Quotes/Quotes.jsx
+++ src/pages/Quotes/Quotes.jsx
@@ -1,52 +1,45 @@
+import "./Quotes.css";
 import * as React from "react";
+
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { NewNavbar } from "../../components/NewNavbar/NewNavbar";
-import { useEffect, useState } from "react";
+import quotes from "../../common/quotes.json";
+// import { useEffect, useState } from "react";
 
 function Quotes() {
-  const [podaci, setPodaci] = useState([]);
-  useEffect(() => {
-    fetch("https://api.quotable.io/quotes?page=1")
-      .then((url) => {
-        return url.json();
-      })
-      .then((data) => {
-        setPodaci(data.results);
-      });
-  }, []);
-  console.log(podaci);
+  // const [podaci, setPodaci] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.quotable.io/quotes?page=1")
+  //     .then((url) => {
+  //       return url.json();
+  //     })
+  //     .then((data) => {
+  //       setPodaci(data.results);
+  //     });
+  // }, []);
+  // console.log(podaci);
   return (
     <>
       <NewNavbar />
-      {podaci.map((value) => {
-        <Card sx={{ width: 275, boxShadow: "0px 10px 20px 5px black" }}>
-          <CardContent>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.secondary"
-              gutterBottom
-            >
-              {value.author}
-            </Typography>
-            <Typography variant="h5" component="div"></Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              adjective
-            </Typography>
-            <Typography variant="body2">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>;
-      })}
+      <div className="quotes">
+        {quotes.map((value) => {
+          return (
+            <Card sx={{ width: 275, boxShadow: "0px 10px 20px 5px black" }}>
+              <CardContent>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  {value.author}
+                </Typography>
+                <Typography variant="body3">{value.content}</Typography>
+                <Typography sx={{ mb: 1.5 }}>{value.year}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </div>
     </>
   );
 }
