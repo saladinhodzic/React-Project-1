587b6d8884cc8037aa57344fc2947f255d3819a7
saladinhodzic <hsaladin06@gmail.com>
2024-06-28 20:05:05 +0200

    Introduction to Context state managment

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+const AppContext = createContext();
+
+function ContextProvider({ children }) {
+  const [isActive, setIsActive] = useState(() => {
+    const state = localStorage.getItem(`${id}`);
+    const parse = JSON.parse(state);
+    return parse || null;
+  });
+
+  const values = {
+    isActive,
+    setIsActive,
+  };
+  return <AppContext.Provider value={values}>{children}</AppContext.Provider>;
+}
+
+export { AppContext, ContextProvider };
