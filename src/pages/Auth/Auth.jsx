bee1f74cf6811c3e12ade28362ece21ee49184de
saladinhodzic <hsaladin06@gmail.com>
2024-06-30 16:15:19 +0200

    Changing the content on login and signUp

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -4,14 +4,24 @@ import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import { SignUp } from "../../components/Signup/Signup";
 import { LogIn } from "../../components/Login/Login";
+import { useState } from "react";
 
 export function Auth() {
+  const [signUp, login] = useState(true);
+  const [value, setValue] = useState(0);
+  function handleClick(e, value) {
+    login(!signUp);
+    setValue(value);
+  }
+
   return (
     <>
-      <Box>
-        <Tabs>
-          <Tab />
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={value} onChange={handleClick} centered>
+          <Tab label="Sign up" />
+          <Tab label="Login" />
         </Tabs>
+        {signUp ? <SignUp /> : <LogIn />}
       </Box>
     </>
   );
