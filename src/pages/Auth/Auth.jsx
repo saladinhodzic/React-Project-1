8c72e613c6c4d9b15a0c6a0b6cc3cb29f13cd262
saladinhodzic <hsaladin06@gmail.com>
2024-07-02 13:25:47 +0200

    Starting the Formik

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,40 +1,16 @@
 import "./Auth.css";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
 import { SignUp } from "../../components/Signup/Signup";
 import { LogIn } from "../../components/Login/Login";
 import { useState } from "react";
 import { NewNavbar } from "../../components/NewNavbar/NewNavbar";
-
+import { Formik } from "formik";
 export function Auth() {
-  const [signUp, login] = useState(true);
-  const [value, setValue] = useState(0);
-  function handleClick(e, value) {
-    login(!signUp);
-    setValue(value);
-  }
-
   return (
-    <div className="wrapper">
-      <NewNavbar />
-      <div className="auth">
-        <Box
-          sx={{
-            borderBottom: 1,
-            borderColor: "divider",
-            backgroundColor: "white",
-            width: "30%",
-            minHeight: "200px",
-          }}
-        >
-          <Tabs value={value} onChange={handleClick} centered>
-            <Tab label="Sign up" />
-            <Tab label="Login" />
-          </Tabs>
-          {signUp ? <SignUp /> : <LogIn />}
-        </Box>
-      </div>
-    </div>
+    <Formik>
+      <form>
+        <label>Email:</label>
+        <input type="email" />
+      </form>
+    </Formik>
   );
 }
