04a28721c6611981cf17626f11b3b2381fe9c25b
saladinhodzic <hsaladin06@gmail.com>
2024-07-05 11:46:28 +0200

    Adding styles and animation

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,8 @@
 import "./About.css";
 import saki from "../../assets/saki.png";
+import dzenan from "../../assets/dzenan.png";
 import { NewNavbar } from "../../components/NewNavbar/NewNavbar";
+import { Footer } from "../../components/Footer/Footer";
 
 export function About() {
   return (
@@ -12,14 +14,33 @@ export function About() {
           <br /> Together, We Make a Difference.
         </h2>
       </div>
-      <div className="person">
-        <img className="profilna" src={saki} alt="" width="20px" />
-        <p className="ime">Saladin Hodzic</p>
-        {/* <p className="description">
-          Graduiting graphic design was big step towards making my UI design
-          looking as good as i can
-        </p> */}
+      <div className="team">
+        <div className="person">
+          <img className="profilna" src={dzenan} alt="" width="20px" />
+          <p className="person-name">Dzenan Kosuta</p>
+          <p className="description">
+            Graduiting graphic design was big step towards making my UI design
+            looking as good as i can
+          </p>
+        </div>
+        <div className="person">
+          <img className="profilna" src={saki} alt="" width="20px" />
+          <p className="person-name">Saladin Hodzic</p>
+          <p className="description">
+            Graduiting graphic design was big step towards making my UI design
+            looking as good as i can
+          </p>
+        </div>
+        <div className="person">
+          <img className="profilna" src={saki} alt="" width="20px" />
+          <p className="person-name">Saladin Hodzic</p>
+          <p className="description">
+            Graduiting graphic design was big step towards making my UI design
+            looking as good as i can
+          </p>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
